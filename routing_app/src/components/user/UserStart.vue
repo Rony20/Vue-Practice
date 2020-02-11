23e4d341e71c4fc1e549160ba8cf31d1fb1ff72a
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-02-11 18:28:36 +0530

    routing_app is added

---
+++ routing_app/src/components/user/UserStart.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p> Please Select User </p>
+        <hr>
+        <ul class="list-group">
+            <router-link 
+                class="list-group-item" 
+                to="user/1"
+                style="cursor: pointer" 
+                tag="li"> User 1 </router-link>
+            <router-link 
+                class="list-group-item"
+                to="user/2" 
+                style="cursor: pointer" 
+                tag="li"> User 2 </router-link>
+            <router-link 
+                class="list-group-item"
+                to="user/3" 
+                style="cursor: pointer" 
+                tag="li"> User 3 </router-link>
+        </ul>
+    </div>
+</template>
