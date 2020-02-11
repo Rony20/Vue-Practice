43036afb823f8425543f380c76b390c3c82bd4e0
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-02-11 18:09:03 +0530

    all vue folders are added.

---
+++ routing_app/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>The Home Page</h1>
+    <hr/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Dolor ab soluta in nostrum laboriosam rem molestiae rerum optio enim ut sed,
+        accusamus quaerat animi adipisci necessitatibus a expedita eos magnam!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
---
+++ routing_app/src/components/user/UserDetail.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="main">
+    <h3> User Details </h3>
+    <p> User Loaded Has Id: {{ $route.params.id }}</p>
+    <router-link 
+        tag="button" 
+        :to="{ name: 'userEdit', params: {id: $route.params.id}, query: { locale: 'en', q: 100} }"
+        class="btn btn-primary"> Edit User </router-link>
+</div>
+<!-- This is also be used as 'to' attribute in router-link.  -->
+<!-- :to="'/user/' + $route.params.id + '/edit'" -->
+</template>
