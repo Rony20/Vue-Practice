fa8646aaa1ee54341454efe37ab1fd879e651a18
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-02-11 18:24:24 +0530

    Folders are removed

--- routing_app/src/components/user/UserEdit.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="main">
-        <h3> Edit The User {{ $route.params.id }}</h3>
-        <p>Locale : {{ $route.query.locale }}</p>
-        <p> Analytics : {{ $route.query.q }}</p>
-    </div>
-</template>
