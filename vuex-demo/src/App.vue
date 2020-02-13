1bec9b6b5d4068d67876ed23f38fb7b65e8dd7f3
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-02-13 16:46:37 +0530

    Veux is used with computed property and vuex states

---
+++ vuex-demo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>VueX</h1>
+        <app-result></app-result>
+        <app-another-result></app-another-result>
+        <hr>
+        <app-Counter></app-Counter>
+      </div>
+    </div>    
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+import Result from './components/Result.vue'
+import AnotherResult from './components/AnotherResult.vue'
+
+export default {
+  name: 'App',
+  components:{
+    appCounter: Counter,
+    appResult: Result,
+    appAnotherResult: AnotherResult
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuex-demo/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="increment"> Increment </button> &nbsp;
+    <button class="btn btn-primary" @click="decrement"> Decrement </button>
+  </div>
+</template>
+
+<script>
+export default {
+ methods:
+ {
+   increment()
+   {
+     this.$store.state.counter++
+   },
+   decrement()
+   {
+     this.$store.state.counter--
+   }
+ }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
