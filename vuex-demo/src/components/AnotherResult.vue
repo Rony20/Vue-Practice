aa0f181caf580aa7f54e5886ccab07b4d486d3a3
Rohan Borakhatariya <rohan.borakhatariya@CDSYS.LOCAL>
2020-02-14 11:57:32 +0530

    Adding getters for more reliable access to multiple computed properties. But still not used scoped computed properties.

--- vuex-demo/src/components/AnotherResult.vue
+++ vuex-demo/src/components/AnotherResult.vue
@@ -1,13 +1,17 @@
-<template>
-    <p> Counter is : {{ counter }}</p>
+<template> 
+    <div>
+    <p> Counter is : {{ doubleCounter }}</p>
+    <p> Number of clicks : {{ stringCounter }} </p>
+    </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
-    computed: {
-        counter(){
-            return this.$store.getters.doubleCounter
-        }
+    computed: mapGetters([
+        'doubleCounter',
+        'stringCounter'
+    ])
     }
-}
 </script>
+// We can aslo use object instead of list in mapGetters. But still one problem is there. We cannot use our own computed property here.
